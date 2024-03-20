5480ffa60b2ca3ebd6987f932b6f3072b859b75a
Martin Camitz <martin.camitz@omegapoint.se>
2024-03-20 22:43:04 +0100

    Minimal API

---
+++ LinqSample/AsyncLinqDemo.cs
@@ -0,0 +1,18 @@
+﻿namespace LinqSample;
+
+public class AsyncLinqDemo
+{
+    int[] ids = Enumerable.Range(1, 10).ToArray();
+    
+    public async Task<List<string>> GetCharacters()
+    {
+        List<string> characters = new List<string>();
+        foreach (var number in ids)
+        {
+            string url = $"https://api.sampleapis.com/futurama/characters/{number}";
+            string response = await new HttpClient().GetStringAsync(url);
+            characters.Add(response);
+        }
+        return characters;
+    }
+}
---
+++ LinqSample/Program.cs
@@ -0,0 +1,39 @@
+﻿var knuth = "Programming is the art of telling another human being what one wants the computer to do.";
+
+var knuthArray = knuth.Split(' ');
+
+Console.WriteLine(knuthArray.Average(x => x.Length));
+Console.WriteLine(knuthArray.Variance(x => x.Length));
+
+knuthArray
+    .OrderBy(x => x.Length)
+    .ToList()
+    .ForEach(Console.WriteLine);
+
+knuthArray
+    .Where(x => x.Length > 3)
+    .OrderByDescending(x => x.Length)
+    .SelectMany(x => x.ToCharArray())
+    .ToList()
+    .ForEach(Console.WriteLine);
+
+(
+        from x in knuthArray
+        where x.Length > 3
+        orderby x.Length descending
+
+        from y in x
+        select x.ToCharArray()
+    )
+    .ToList()
+    .ForEach(Console.WriteLine);
+
+public static class StatsExtensions
+{
+    public static double Variance<TSource>(this IEnumerable<TSource> source, Func<TSource, double> selector)
+    {
+        var enumerable = source as TSource[] ?? source.ToArray();
+        var avg = enumerable.Average(selector);
+        return enumerable.Sum(x => Math.Pow(selector(x) - avg, 2)) / enumerable.Length;
+    }
+}
